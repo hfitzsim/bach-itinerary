b96f094c970d8bc3353f9aa7e74c115b33ac6393
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2026-01-28 18:26:37 -0500

    this or that

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Itinerary } from './Itinerary';
 import { Jeopardy } from './Jeopardy';
+import { ThisOrThatPage } from './ThisOrThat';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Itinerary />} />
 			<Route path="/jeopardy" element={<Jeopardy />} />
+			<Route path="/this-or-that" element={<ThisOrThatPage />} />
 		</Routes>
 	);
 }
---
+++ src/ThisOrThatData.ts
@@ -0,0 +1,73 @@
+export type ThisOrThatQuestion = {
+	id: string;
+	question: string;
+	optionA: string;
+	optionB: string;
+	correct: string;
+};
+
+export const thisOrThatQuestions: ThisOrThatQuestion[] = [
+	{
+		id: '1',
+		question: 'Sleep in late or Wake up early?',
+		optionA: 'Sleep in late',
+		optionB: 'Wake up early',
+		correct: '',
+	},
+	{
+		id: '2',
+		question: 'Cook dinner or Do the laundry?',
+		optionA: 'Cook dinner',
+		optionB: 'Do the laundry',
+		correct: '',
+	},
+	{
+		id: '3',
+		question: 'Tropical vacation or European adventure?',
+		optionA: 'Tropical vacation',
+		optionB: 'European adventure',
+		correct: '',
+	},
+	{
+		id: '4',
+		question: 'Sushi takeout or Chinese takeout?',
+		optionA: 'Sushi takeout',
+		optionB: 'Chinese takeout',
+		correct: '',
+	},
+	{
+		id: '5',
+		question: 'Own a dog or Own a cat?',
+		optionA: 'Own a dog',
+		optionB: 'Own a cat',
+		correct: '',
+	},
+	{
+		id: '6',
+		question: 'Chocolate or Vanilla?',
+		optionA: 'Chocolate',
+		optionB: 'Vanilla',
+		correct: '',
+	},
+	{
+		id: '7',
+		question: 'Sip tea or Drink coffee?',
+		optionA: 'Sip tea',
+		optionB: 'Drink coffee',
+		correct: '',
+	},
+	{
+		id: '8',
+		question: 'Brunch or Happy hour?',
+		optionA: 'Brunch',
+		optionB: 'Happy hour',
+		correct: '',
+	},
+	{
+		id: '9',
+		question: 'Peloton ride or Run outside?',
+		optionA: 'Peloton ride',
+		optionB: 'Run outside',
+		correct: '',
+	},
+];
