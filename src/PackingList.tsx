f43b0902a495ce61fb5a936c03b89dde1ed0c7f1
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2026-01-27 20:25:18 -0500

    add details to packing list

--- src/PackingList.tsx
+++ src/PackingList.tsx
@@ -3,14 +3,14 @@ import { List } from '@mantine/core';
 const PackingList = () => {
 	return (
 		<List>
-			<List.Item>Warm layers (coat, scarf, etc.)</List.Item>
-			<List.Item>Cute and comfy walking shoes</List.Item>
+			<List.Item>Warm layers (snow pants, coat, scarf, etc.)</List.Item>
+			<List.Item>Comfy, warm, and water resistant snow boots/walking shoes</List.Item>
 			<List.Item>Cozy pajama bottoms or shorts</List.Item>
 			<List.Item>
 				Swimsuit, sandals, reusable water bottle, and a waterproof bag if you have one
 			</List.Item>
-			<List.Item>Workout clothes for pilates (light blue or grey/cool toned)</List.Item>
-			<List.Item>One going out outfit (pink or red for Galentines)</List.Item>
+			<List.Item>Workout clothes for pilates (light dusty blue, or greyish blue tones)</List.Item>
+			<List.Item>One going out outfit (pink and/or red (cherry, burgundy))</List.Item>
 		</List>
 	);
 };
